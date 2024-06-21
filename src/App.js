bab83187e7da0c88d42880cd175be8618b10dfe5
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-06-21 18:15:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BitcoinCurrency from './bitcoin';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bitcoin Converter</h1> 
+        <BitcoinCurrency />
       </header>
     </div>
   );
---
+++ src/bitcoin.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function BitcoinCurrency(){
+return(
+<div>
+<input type='number'/>
+<div>
+    <option value='USD'>USD</option>
+    <option value='EUR'>EUR</option>
+</div>
+
+
+
+</div>
+
+
+)
+
+
+
+}
+
+
+
+
+
+
+export default BitcoinCurrency
