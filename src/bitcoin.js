75455eba41bd4dc7c9c72e86b1ad3273bb183b1d
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-06-22 17:46:51 +0100

    add useState

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -1,6 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function BitcoinCurrency(){
+
+
+
+const  BitcoinCurrency = () => {
+// Estado para almacenar la tasa de converscion 
+const [cryptoRate , setCryptoRate] = useState({});
+// Estado para almacenar el monto ingresado por el usuario 
+const [amount , setAmount] = useState(1);
+// Estado para almacenar la moneda seleccionada (USD O EUR)
+const [fiatCurrency , setFiatCurrency] = useState('USD');
+// Estado para almacenar el monto convertido
+const [convertedAmount, setConvertedAmount] = useState(0);
+
+ // Hook de efecto para obtener la tasa de conversión desde la API 
+useEffect(() => {
+const fetchCryptoRate = async () => {
+
+try {
+const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,eur');
+setCryptoRate(response.data.bitcoin);
+} catch (error) {
+console.error("Error fetching the crypto rates", error);
+
+}
+};
+
+fetchCryptoRate();
+
+},[]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 return(
 <div>
 <input type='number'/>
